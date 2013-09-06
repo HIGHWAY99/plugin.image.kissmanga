ec1e1bb929557e0aa1350b12bad21292b9330c95
HIGHWAY99 <SilentHighway@GMail.com>
2013-09-06 16:18:14 -0500

    v0.2.9

--- teh_tools.py
+++ teh_tools.py
@@ -1721,7 +1721,7 @@ def GRABMETA(name,types):
 			infoLabels={'rating': meta['rating'],'duration': meta['duration'],'genre': meta['genre'],'mpaa':"rated %s"%meta['mpaa'],'plot': meta['plot'],'title': meta['title'],'writer': meta['writer'],'cover_url': meta['cover_url'],'director': meta['director'],'cast': meta['cast'],'backdrop_url': meta['backdrop_url'],'backdrop_url': meta['backdrop_url'],'tmdb_id': meta['tmdb_id'],'year': meta['year']}
 		elif ('tvshow' in type):
 			meta=grab.get_meta('tvshow',name,'','',None,overlay=6)
-			print meta
+			#print meta
 			infoLabels={'rating': meta['rating'],'genre': meta['genre'],'mpaa':"rated %s"%meta['mpaa'],'plot': meta['plot'],'title': meta['title'],'cover_url': meta['cover_url'],'cast': meta['cast'],'studio': meta['studio'],'banner_url': meta['banner_url'],'backdrop_url': meta['backdrop_url'],'status': meta['status']}
 		else: infoLabels={}
 	else: infoLabels={}
